6f06589486bf17e4918374df6e7cda6bf73e9b78
perfdawg <d6zhjrc5dz@privaterelay.appleid.com>
2025-11-23 23:28:58 +0530

    strsplit init

---
+++ strsplit/src/lib.rs
@@ -0,0 +1,37 @@
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
+}
+
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
+}
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delim = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delim)
+        } else if self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ");
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
+}
