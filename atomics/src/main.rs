60cc89527c17da0c8fd0c41c4ac9f51b025ee257
perfdawg <d6zhjrc5dz@privaterelay.appleid.com>
2025-12-09 22:21:38 +0530

    atomics with compare exchange

--- atomics/src/main.rs
+++ atomics/src/main.rs
@@ -1,3 +1,58 @@
+use std::{
+    cell::UnsafeCell,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+const LOCKED: bool = true;
+const UNLOCKED: bool = false;
+
+struct Mutex<T> {
+    locked: AtomicBool,
+    v: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
+impl<T> Mutex<T> {
+    pub fn new(t: T) -> Self {
+        Self {
+            locked: AtomicBool::new(UNLOCKED),
+            v: UnsafeCell::new(t),
+        }
+    }
+
+    pub fn with_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+        while self
+            .locked
+            .compare_exchange(UNLOCKED, LOCKED, Ordering::Relaxed, Ordering::Relaxed)
+            .is_err()
+        {
+            while self.locked.load(Ordering::Relaxed) == LOCKED {}
+        }
+        // SAFETY: we hold the lock, therefore we can create a mutable reference.
+        let ret = f(unsafe { &mut *self.v.get() });
+        self.locked.store(UNLOCKED, Ordering::Relaxed);
+        ret
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let l: &'static _ = Box::leak(Box::new(Mutex::new(0)));
+    let handles: Vec<_> = (0..1000)
+        .map(|_| {
+            std::thread::spawn(move || {
+                for _ in 0..10000 {
+                    l.with_lock(|v| {
+                        *v += 1;
+                    })
+                }
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    assert_eq!(l.with_lock(|v| *v), 1000 * 10000);
 }
