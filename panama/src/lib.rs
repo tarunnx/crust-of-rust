36106d0722786fe3c2b542fb625f78923db2108d
perfdawg <d6zhjrc5dz@privaterelay.appleid.com>
2025-12-11 03:13:12 +0530

    panama basic impl

--- panama/src/lib.rs
+++ panama/src/lib.rs
@@ -1,20 +1,57 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Condvar, Mutex},
+};
 
 pub struct Sender<T> {
     inner: Arc<Inner<T>>,
 }
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Sender {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+impl<T> Sender<T> {
+    pub fn send(&self, t: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(t);
+        drop(queue);
+        self.inner.available.notify_one();
+    }
+}
+
 pub struct Receiver<T> {
     inner: Arc<Inner<T>>,
 }
 
+impl<T> Receiver<T> {
+    pub fn recv(&self) -> T {
+        let mut queue = self.inner.queue.lock().unwrap();
+        loop {
+            if let Some(res) = queue.pop_front() {
+                return res;
+            } else {
+                queue = self.inner.available.wait(queue).unwrap();
+            }
+        }
+    }
+}
+
 struct Inner<T> {
-    queue: Mutex<Vec<T>>,
+    queue: Mutex<VecDeque<T>>,
+    available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::new(Vec::new()),
+        queue: Mutex::new(VecDeque::new()),
+        available: Condvar::new(),
     };
+
     let inner = Arc::new(inner);
 
     (
@@ -25,4 +62,16 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
             inner: inner.clone(),
         },
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ping_pong() {
+        let (tx, rx) = channel();
+        tx.send(42);
+        assert_eq!(rx.recv(), 42)
+    }
 }
